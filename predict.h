0aecd8078ea851058b77bdd98aa4cc6925e8e57e
Mikael Voss <mikael@illdefined.org>
2010-01-06 13:20:10 +0000

    Rename macro argument

--- predict.h
+++ predict.h
@@ -2,12 +2,30 @@
 #ifndef PREDICT_H
 #define PREDICT_H
 
+/**
+ * \def likely(expr)
+ * \brief Predict expression to be true
+ *
+ * \param expr expression
+ *
+ * \return truth value of expression
+ */
+
+/**
+ * \def unlikely(expr)
+ * \brief Predict expression to be false
+ *
+ * \param expr expression
+ *
+ * \return truth value of expression
+ */
+
 #if defined(__GNUC__) || defined(__clang__)
-#define likely(x)   __builtin_expect(!!(x), 1)
-#define unlikely(x) __builtin_expect(!!(x), 0)
+#define likely(expr)   __builtin_expect(!!(expr), 1)
+#define unlikely(expr) __builtin_expect(!!(expr), 0)
 #else
-#define likely(x)   (x)
-#define unlikely(x) (x)
+#define likely(expr)   (expr)
+#define unlikely(expr) (expr)
 #endif
 
 #endif
